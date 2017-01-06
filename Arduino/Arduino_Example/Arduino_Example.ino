fd996c38c289b68c26de549bfad0de0db4078d04
BardinPetr <bardin.petr@gmail.com>
2017-01-06 22:00:50 +0300

    Add: example codes for arduino and esp

---
+++ Arduino/Arduino_Example/Arduino_Example.ino
@@ -0,0 +1,61 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#include "DHT.h"
+#include <Wire.h>
+#include <BH1750.h>
+
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+EthernetServer server(80);
+DHT dht;
+BH1750 lightMeter;
+
+void setup() {
+  Serial.begin(9600);
+
+  Ethernet.begin(mac);
+  server.begin();
+  Serial.print("server is at ");
+  Serial.println(Ethernet.localIP());
+
+  Wire.begin();
+  dht.setup(2);
+  lightMeter.begin();
+}
+
+
+void loop() {
+  EthernetClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  while(!client.available()){
+    delay(1);
+  }
+  
+  String req = client.readStringUntil('\r');
+  client.flush();
+
+  int humidity = dht.getHumidity();
+  int temperature = dht.getTemperature();
+  int light = lightMeter.readLightLevel();;
+  
+  if (req.indexOf("/temperature") != -1)
+    client.print(temperature);
+  else if (req.indexOf("/humidity") != -1)
+    client.print(humidity);
+  else if (req.indexOf("/light") != -1)
+    client.print(light);
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  client.flush();
+
+  delay(1);
+  Serial.println("Client disonnected");
+}
+
---
+++ Arduino/ESP8266_Example/ESP8266_Example.ino
@@ -0,0 +1,64 @@
+#include <ESP8266WiFi.h>
+#include "DHT.h"
+
+const char* ssid = "your-ssid";
+const char* password = "your-password";
+
+WiFiServer server(80);
+DHT dht;
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  
+  server.begin();
+  Serial.println("Server started");
+
+  Serial.println(WiFi.localIP());
+
+  dht.setup(2);
+}
+
+void loop() {
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+  
+  while(!client.available()){
+    delay(1);
+  }
+  
+  String req = client.readStringUntil('\r');
+  client.flush();
+
+  int humidity = dht.getHumidity();
+  int temperature = dht.getTemperature();
+  
+  if (req.indexOf("/temperature") != -1)
+    client.print(temperature);
+  else if (req.indexOf("/humidity") != -1)
+    client.print(humidity);
+  else {
+    Serial.println("invalid request");
+    client.stop();
+    return;
+  }
+
+  client.flush();
+
+  delay(1);
+  Serial.println("Client disonnected");
+}
+
